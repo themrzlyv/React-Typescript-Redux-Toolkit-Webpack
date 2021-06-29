c5bd3c9cbc46748e7dba82016153f9cadd2c0175
Emil Sultanov <emil.sultanov@cybernet.az>
2021-06-29 16:38:02 +0400

    Add absolute aliases

--- src/pages/MainPages.tsx
+++ src/pages/MainPages.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Switch , Route} from 'react-router-dom'
-import Home from './Home/Home'
-import NotFound from './NotFound/NotFound'
+import { Switch, Route } from 'react-router-dom'
+import Home from '@/pages/Home/Home'
+import NotFound from '@/pages/NotFound/NotFound';
 
-const MainPages:React.FC = () => {
+const MainPages: React.FC = () => {
     return (
         <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route component={NotFound}/>
+            <Route exact path="/" component={Home} />
+            <Route component={NotFound} />
         </Switch>
     )
 }
